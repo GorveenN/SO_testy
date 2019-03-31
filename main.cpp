989843f6c6875f891bfe380497640eb7c96dc1da
Piotr Karpinski <gorveenn@gmail.com>
2019-03-31 12:00:34 +0200

    Added bf tests

--- main.cpp
+++ main.cpp
@@ -3,13 +3,8 @@
 #include <filesystem>
 #include <fstream>
 #include <cassert>
-#include <future>
-#include <thread>
 
 uint64_t N;
-
-
-namespace fs = std::filesystem;
 
 extern "C" uint64_t euron(uint64_t n, char const *prog);
 
@@ -22,53 +17,59 @@ extern "C" void put_value(uint64_t n, uint64_t v) {
     assert(n < N);
 }
 
-void test(uint64_t id, const char * com, std::promise<uint64_t> && prom) {
-    prom.set_value(euron(id, com));
+struct eur {
+    uint64_t id;
+    const char *s;
+    uint64_t result;
+    uint64_t expected;
+};
+
+void *test_euron(void *s) {
+    eur *e = (eur *) s;
+    e->result = euron(e->id, e->s);
+    return NULL;
+}
+
+void test(eur *e) {
+    pthread_t t[N];
+    for (uint64_t i = 0; i < N; ++i) {
+        pthread_create(t + i, NULL, test_euron, (void*)(e + i));
+    }
+    for (uint64_t i = 0; i < N; ++i) {
+        pthread_join(t[i], NULL);
+    }
 }
 
 
 int main(int argc, char *argv[]) {
     N = atoll(argv[1]);
     uint64_t test_count = 0;
-
-    const char * strings[N];
-    uint64_t results[N];
+    eur eurons[N];
+    std::string strings[N];
 
     std::string input = "./tests/" + std::to_string(N) + "/in/";
     std::string output = "./tests/" + std::to_string(N) + "/out/";
-
-    std::vector<std::string> failed_name;
     uint64_t failed_n = 0;
 
-    for (const auto & entry : fs::directory_iterator(input)) {
+    for (const auto &entry : std::filesystem::directory_iterator(input)) {
+        std::cout << "test: " << test_count << std::endl;
         test_count++;
         std::string tmp = entry.path().filename();
         std::string filename = tmp.substr(0, tmp.length() - 3);
         std::ifstream in, out;
 
-        std::promise<uint64_t> promises[N];
-        std::future<uint64_t> futures[N];
-        std::thread threads[N];
-
-        in.open (input + tmp);
-        out.open (output + filename + ".out");
+        in.open(input + tmp);
+        out.open(output + filename + ".out");
         for (uint64_t i = 0; i < N; i++) {
-            std::string command;
-
-            out >> results[i];
-            in >> command;
-            futures[i] = promises[i].get_future();
-            strings[i] = command.c_str();
-
-            threads[i] = std::thread(&test, i, strings[i], std::move(promises[i]));
-            threads[i].join();
+            out >> eurons[i].expected;
+            in >> strings[i];
+            eurons[i].s = strings[i].c_str();
+            eurons[i].id = i;
         }
+        test(eurons);
 
-
-        size_t i;
-        for (i = 0; i < N; i++) {
-            if (results[i] != futures[i].get()) {
-                failed_name.push_back(filename);
+        for (uint64_t i = 0; i < N; i++) {
+            if (eurons[i].expected != eurons[i].result) {
                 failed_n++;
                 break;
             }
@@ -77,9 +78,4 @@ int main(int argc, char *argv[]) {
 
     std::cout << "passed: " << test_count - failed_n << std::endl;
     std::cout << "failed: " << failed_n << std::endl;
-    if (failed_n > 0) {
-        for (std::string& s : failed_name) {
-            std::cout << s << std::endl;
-        }
-    }
 }
